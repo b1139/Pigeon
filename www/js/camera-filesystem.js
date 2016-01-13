cf9b36dd81c8e2ed91e42243cf11e59437088486
guruSathish <sathishkumarb1139@gmail.com>
2016-01-13 12:52:55 +0530

    Applied the fetch image in chat page and changed the camara-filesystem.js

--- www/js/camera-filesystem.js
+++ www/js/camera-filesystem.js
@@ -1,107 +1,104 @@
-// deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        console.log('Received Event: ' + id);
-        // request a new file system object then pass that to the gotFS() method.
-        // gotFS() is a simple setter for a global var that the FileIO methods will use later.
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, FileIO.gotFS, FileIO.errorHandler);
-    }
-	
-/*	$(".fa-file-image-o").on('click', function() {*/
-getPhoto(pictureSource.PHOTOLIBRARY);
-/*});*/
- 
-function getPhoto(source) { 
-    // Retrieve image file location from specified source
-	alert('get Photo');
-    navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-                                destinationType: destinationType.FILE_URI,
-                                saveToPhotoAlbum: false,
-                                sourceType: source,
-                                allowEdit: true });
-}
- 
 // Called when a photo is successfully retrieved
-function onPhotoURISuccess(imageURI) {
-    FileIO.updateCameraImages(imageURI);
-}
- 
-// Called if something bad happens.
-function onFail(message) {
-    console.log('Failed because: ' + message);
-}
+    //
+    function onPhotoDataSuccess(imageData) {
+      // Get image handle
+      //
+      var smallImage = document.getElementById('smallImage');
 
-// set some globals
-var gImageURI = '';
-var gFileSystem = {};
- 
-var FileIO = {
- 
-// sets the filesystem to the global var gFileSystem
- gotFS : function(fileSystem) {
-      gFileSystem = fileSystem;
- },
- 
-// pickup the URI from the Camera edit and assign it to the global var gImageURI
-// create a filesystem object called a 'file entry' based on the image URI
-// pass that file entry over to gotImageURI()
-updateCameraImages : function(imageURI) {
-        gImageURI = imageURI;
-        window.resolveLocalFileSystemURI(imageURI, FileIO.gotImageURI, FileIO.errorHandler);
-    },
- 
-// pickup the file entry, rename it, and move the file to the app's root directory.
-// on success run the movedImageSuccess() method
- gotImageURI : function(fileEntry) {
-       var newName = "thumbnail_" + gCurrentFlo + ".jpg";
-       fileEntry.moveTo(gFileSystem.root, newName, FileIO.movedImageSuccess, FileIO.errorHandler);
- },
- 
-// send the full URI of the moved image to the updateImageSrc() method which does some DOM manipulation
- movedImageSuccess : function(fileEntry) {
-      updateImageSrc(fileEntry.fullPath);
- },
- 
-// get a new file entry for the moved image when the user hits the delete button
-// pass the file entry to removeFile()
- removeDeletedImage : function(imageURI){
-      window.resolveLocalFileSystemURI(imageURI, FileIO.removeFile, FileIO.errorHandler);
- },
- 
-// delete the file
- removeFile : function(fileEntry){
-      fileEntry.remove();
- },
- 
-// simple error handler
- errorHandler : function(e) {
-       var msg = '';
-       switch (e.code) {
-       case FileError.QUOTA_EXCEEDED_ERR:
-               msg = 'QUOTA_EXCEEDED_ERR';
-               break;
-        case FileError.NOT_FOUND_ERR:
-               msg = 'NOT_FOUND_ERR';
-               break;
-        case FileError.SECURITY_ERR:
-               msg = 'SECURITY_ERR';
-               break;
-        case FileError.INVALID_MODIFICATION_ERR:
-               msg = 'INVALID_MODIFICATION_ERR';
-               break;
-        case FileError.INVALID_STATE_ERR:
-               msg = 'INVALID_STATE_ERR';
-               break;
-        default:
-               msg = e.code;
-        break;
- };
-       console.log('Error: ' + msg);
- }
-}
+      // Unhide image elements
+      //
+      smallImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      smallImage.src = "data:image/jpeg;base64," + imageData;
+    }
+    
+	// Called when a photo is successfully retrieved
+    //
+    function onPhotoFileSuccess(imageURI) {alert('file');
+      // Get image handle
+      console.log(JSON.stringify(imageData));
+      
+   	  // Get image handle
+      //
+      var smallImage = document.getElementById('smallImage');
+
+      // Unhide image elements
+      //
+      smallImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      smallImage.src = imageData;
+    }
+
+    // Called when a photo is successfully retrieved
+    //
+    function uploadPhoto(imageURI) { 
+      var options = new FileUploadOptions();
+	  options.fileKey = "file";
+	  options.fileName = imageURI.substr(imageURI.lastIndexOf('/')+1);
+	  options.mimeType = "image/jpeg";
+	  
+	  pictureSource = imageURI.substr(imageURI.lastIndexOf('/')+1);
+	  alert("Name:"+pictureSource);
+	  alert(imageURI);
+	  var params = new Object();
+      params.value1 = "test";
+      params.value2 = "param";
+ 		
+      options.params = params;
+      options.chunkedMode = false;// If it is not set the PHP server won't able to read this image'
+	  var ft = new FileTransfer();
+	  ft.upload(imageURI,getBaseURL()+"?rquest=uploadImage",win,fail,options);
+	  
+	  alert('completed uploading');
+    }
+
+    function win(r){
+		alert('success'); 
+		alert("Response = " + r.response);
+		alert("Sent = " + r.bytesSent);
+		// var largeImage = document.getElementById('largeImage');
+
+      // Unhide image elements
+      //
+      //largeImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      //largeImage.src = getSiteURL()+"/rest/uploads/images/"+pictureSource;
+		
+	}
+	function fail(error){
+		alert('Failed');
+	}
+    // A button will call this function
+    //
+    function capturePhotoWithData() {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
+    }
+
+    function capturePhotoWithFile() {
+        navigator.camera.getPicture(onPhotoFileSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI });
+    }
+    
+    // A button will call this function
+    //
+    function getPhoto(source) {alert('getPhoto');
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(uploadPhoto, onFail, { quality: 50, 
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+    }
+
+    // Called if something bad happens.
+    // 
+    function onFail(message) {
+      alert('Failed because: ' + message);
+    }
