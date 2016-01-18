dbb49eb63b8f2319381b9991300e2b2afc2fe926
guruSathish <sathishkumarb1139@gmail.com>
2016-01-18 16:12:21 +0530

    Testing image uploading with db details

--- www/js/camera-filesystem.js
+++ www/js/camera-filesystem.js
@@ -1,6 +1,8 @@
 	 var pictureSource;   // picture source
     var destinationType; // sets the format of returned value  
-
+	
+	var toUser;
+	var type;
     // Called when a photo is successfully retrieved
     //
     function uploadPhoto(imageURI) { 
@@ -12,9 +14,11 @@
 	  pictureSource = imageURI.substr(imageURI.lastIndexOf('/')+1);
 	  alert("Name:"+pictureSource);
 	  alert(imageURI);
+	  
 	  var params = new Object();
       params.userId = localStorage.getItem("userId"); 
- 		
+ 	  params.toUser = toUser;
+	  params.type = type;
       options.params = params;
       options.chunkedMode = false;// If it is not set the PHP server won't able to read this image'
 	  var ft = new FileTransfer();
