4872e64bd6f49e4043a06276d59d9696581c353c
guruSathish <sathishkumarb1139@gmail.com>
2016-02-02 15:03:13 +0530

    Testing push notification

---
+++ www/js/notiifcation.js
@@ -0,0 +1,22 @@
+/**
+	Files used to send and retrieve the notification for Pigeon
+*/
+function getBaseURL() {
+    var url = location.href;  // entire url including querystring - also: window.location.href; 
+	if(url.indexOf("http://localhost") != -1){
+		return "http://localhost:8090/pigeon/rest/api.php";
+	}else{
+		return "http://creatustent.com/pigeon/rest/api.php";
+	}
+}
+
+function sendRegistrationId(registrationId){
+	alert(registrationId);
+	$.ajax({
+				type:'POST', 
+				url:getBaseURL()+"?rquest=retrieveRegistrationId",  
+				data:{'registrationId':registrationId},
+				success:function(responseText){  
+				},			
+			});
+}
